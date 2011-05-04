b247d854c996efb16aa6f62ba5e2cabafed20c56
Andrew Higginson <azhigginson@gmail.com>
2011-05-04 12:25:40 +0100

    Properly added elpa dir

---
+++ elpa/lacarte-22.0/lacarte-autoloads.el
@@ -0,0 +1,18 @@
+;;; lacarte-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("lacarte-pkg.el" "lacarte.el") (19886
+;;;;;;  58290 984670))
+
+;;;***
+
+(provide 'lacarte-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lacarte-autoloads.el ends here
---
+++ elpa/lacarte-22.0/lacarte-pkg.el
@@ -0,0 +1,1 @@
+(define-package "lacarte" "22.0" "Execute menu items as commands, with completion." (quote nil))
